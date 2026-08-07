51ea97d27c0d1f2965820385c3034e9546a95ad2
Caitlyn <1423429+CaitlynMainer@users.noreply.github.com>
2026-08-07 16:33:41 -0700

    Complete the double-sneak-click system.

--- src/main/scala/li/cil/oc/common/item/GraphicsCard.scala
+++ src/main/scala/li/cil/oc/common/item/GraphicsCard.scala
@@ -6,7 +6,7 @@ import net.neoforged.neoforge.common.extensions.IItemExtension
 
 
 class GraphicsCard(props: Properties, val tier: Int) extends Item(props) with traits.SimpleItem with traits.ItemTier with traits.GPULike with IItemExtension {
-  override protected def supportsDoubleSneakClick = true
+  override protected def canResetComponentIdentity = true
 
   @Deprecated
   override def getDescriptionId = super.getDescriptionId + tier
